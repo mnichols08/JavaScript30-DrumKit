f2fd1c7004713834196e89e8a45ae9f13a511e16
Mike Nichols <mikenichols410@gmail.com>
2019-07-18 03:56:05 -0400

    added replay button

--- index.js
+++ index.js
@@ -1,39 +1,60 @@
-function keyNoise(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`button.key[data-key="${e.keyCode}"]`);
-    if(!audio) return;  // stops the function from running
-    makeNoise(audio,key);
-  }
-  
-  function clickNoise(e){
-    e.preventDefault();
-    if (isNaN(e.path[1].outerHTML.slice(18,21).charAt(2))){
-    e = Number(e.path[1].outerHTML.slice(18,20));
-    } else {
-    e = Number(e.path[1].outerHTML.slice(18,21));
+(function(global){
+    const keys = document.querySelectorAll('.key');
+    class DrumKit {
+        constructor() {
+            this.state = {
+                memory: []
+            }
+        }
+        makeNoise(audio,key){
+        if(!audio) return;
+        this.state.memory.push(audio);
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add('playing');
+        }
+        removeTransition(e) {
+        if (e.propertyName !== 'transform') return;
+        this.classList.remove('playing');
+        }
     }
-    const audio = document.querySelector(`audio[data-key="${e}"]`);
-    const key = document.querySelector(`button.key[data-key="${e}"]`);
-    if(!audio) return;  // stops the function from running
-    makeNoise(audio,key)
-  }
-  
-  function makeNoise(a,k){
-    a.currentTime = 0; 
-    a.play();
-    k.classList.add('playing');
-  }
-  
-  function removeTransition(e){
-    if (e.propertyName !== 'transform') return; // skips if it is not a transition
-    this.classList.remove('playing');
-  }
-  const keys = document.querySelectorAll('button.key');
-  keys.forEach(key => {
-    key.addEventListener('click',clickNoise);
-    key.addEventListener('contextmenu',function(e){
-      e.preventDefault();
-    })
-    key.addEventListener('transitionend',removeTransition);
-  });
-  window.addEventListener('keydown',keyNoise);
+    const drumKit = new DrumKit();
+    class Sound extends DrumKit{
+        constructor(){
+            super()
+        }
+        keyNoise(keypress) {
+        if (keypress.keyCode === 13) {new Sound().playBack()};
+        const audio = document.querySelector(`audio[data-key="${keypress.keyCode}"]`);
+        const key = document.querySelector(`.key[data-key="${keypress.keyCode}"`);
+        drumKit.makeNoise(audio,key)
+        }
+        clickNoise(click) {    
+        click = click.path[1].dataset.key;
+        const audio = document.querySelector(`audio[data-key="${click}"]`);
+        const key = document.querySelector(`button.key[data-key="${click}"`);
+        drumKit.makeNoise(audio,key);
+    }
+        playBack(){
+        const audio = drumKit.state.memory; 
+        let i = 0;
+            setInterval(() => {
+                if (i < audio.length ){    
+                    audio[i].play();
+                    i++;
+                };
+            }, 375) // Replays audio from memory every 375ms 
+        }  
+    }
+    const sound = new Sound()
+    
+    global.addEventListener('keydown', sound.keyNoise);
+    keys.forEach(function(key) {
+        key.addEventListener('click',sound.clickNoise);
+        key.addEventListener('contextmenu',function(e){
+          e.preventDefault();
+        });
+        key.addEventListener('transitionend',drumKit.removeTransition);
+    });
+    document.querySelector('#replay').addEventListener('click', sound.playBack)
+    })(window)
